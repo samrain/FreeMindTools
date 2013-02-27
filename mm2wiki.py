29ffea5c16455d1f6430fb2164bbd9706b2ec386
Hanyu <samrainhan@gmail.com>
2013-02-27 16:59:37 +0800

    增加了mm2wiki程序，主要是用来转成Redmine上wiki格式内容，默认是在app.yaml中定义的目录下生成一个wiki.txt‘

---
+++ mm2wiki.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from __future__ import unicode_literals
+from xml.etree import ElementTree
+import os
+import sys
+import yaml
+from mm2md import MMTransform
+
+def main(argv):
+    confile = file('app.yaml','rb')
+    config = yaml.load(confile)
+    dirconf = 'file_dir'
+    mddir = config[dirconf]['md']
+    mmdir = config[dirconf]['mm']
+
+    textileFilename = 'wiki.txt'
+    mmFilename = '1301绩效结果-淘钢网-IT部.mm'
+    mm = file(os.path.join(mmdir,mmFilename),'rb')
+    textile = file(os.path.join(mddir,textileFilename),'wb')
+    transform = MMTransform()
+    textile.write(transform.mm2textile(mm.read()).encode('utf8'))
+    textile.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
+    # main([])
